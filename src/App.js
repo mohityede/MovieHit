1529be3fc8882b01a0b088c4c9057ba0bf912e82
mohityede <mohityede755@gmail.com>
2023-07-11 11:03:56 +0530

    tested TMDB api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
+import { fetchData } from './utils/api';
 
 function App() {
+  useEffect(() => {
+    getPopular();
+  }, []);
+  const getPopular = async () => {
+    try {
+      const res = await fetchData("/movie/popular");
+      console.log(res);
+    } catch (err) {
+      console.error(err);
+    }
+  }
   return (
     <div className="App">
       App
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_TOKEN = import.meta.env.TMDB_API_TOKEN
-const BASE_URI = "https://api.themoviedb.org/3"
+const API_TOKEN = process.env.REACT_APP_TMDB_API_TOKEN;
+const BASE_URI = "https://api.themoviedb.org/3";
 
 const header = {
     Authorization: "Bearer " + API_TOKEN,
@@ -9,6 +9,7 @@ const header = {
 
 export const fetchData = async (url, params) => {
     try {
+        console.log("token", API_TOKEN)
         const res = await axios.get(BASE_URI + url, {
             headers: header,
             params: params
