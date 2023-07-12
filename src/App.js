b4e5b2866c6da6a45a8aa612f218798b1a5ecaff
mohityede <mohityede755@gmail.com>
2023-07-12 11:22:54 +0530

    react-router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import { useEffect } from 'react'
-import { UseSelector, useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { getApiConfig } from './redux/slices/homeSlice';
-import './App.css';
 import { fetchData } from './utils/api';
+import './App.css';
+
+import Home from './pages/home/Home';
+import PageNotFound from './pages/404/PageNotFound';
+import Details from './pages/details/Details';
+import Explore from './pages/explore/Explore';
+import Search from './pages/search/Search';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   const dispatch = useDispatch();
@@ -22,8 +31,17 @@ function App() {
   }
   return (
     <div className="App">
-      App
-      { url.total_pages }
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/:mediaType/:id' element={ <Details /> } />
+          <Route path='/search/:query' element={ <Search /> } />
+          <Route path='/explore/:mediaType' element={ <Explore /> } />
+          <Route path='*' element={ <PageNotFound /> } />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+import Hero from "./hero/Hero";
+
+const Home = () => {
+    return (
+        <div>
+            <Hero />
+            Home
+        </div>
+    )
+}
+
+export default Home;
